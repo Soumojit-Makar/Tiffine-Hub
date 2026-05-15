7831418f31774ab59f249761dbd4c0ba6a20bbce
Soumojit-Makar <ssoumojitmakar1234@gmail.com>
2026-05-15 18:22:12 +0530

    push1.18

--- api-gateway/src/app.js
+++ api-gateway/src/app.js
@@ -34,7 +34,7 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key', 'x-user-id', 'x-user-role','x-pin-code'],
 }));
 // ── Security & logging ────────────────────────────────────────────────────────
